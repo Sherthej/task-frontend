891616d1ec9a428bce35f837f648c9026517b908
Sherthej <sherlinthejasavarachan@gmail.com>
2025-08-06 16:07:13 +0530

    update commit

--- src/config.jsx
+++ src/config.jsx
@@ -1,3 +1,3 @@
-export const BASE_URL = import.meta.env.VITE_BASE_URL;
+export const BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/api/tasks`;
 // For local development:
 // export const BASE_URL = "http://localhost:5000/api/tasks";
